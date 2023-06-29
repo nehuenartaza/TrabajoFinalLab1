270bdba2b6af5ef909593a46c72ee1f4b8b48e53
EloyRolda <eloyrolda@gmail.com>
2023-06-29 06:52:29 -0300

    Beta 1.0 Open
    Beta pulida al publico

---
+++ SpaceXSystem/SpaceXSystemCode/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Lib.h"
+
+
+int main()
+{
+    readUserData();
+    mainMenu();
+    return 0;
+}
---
+++ SpaceXSystem/readMe.txt
@@ -0,0 +1,5 @@
+Program: Programa como lo recibiria el cliente
+Documentacion: Documentacion.
+SpaceXSystemCode: Codigo Fuente del sistema
+
+Gracias por leer.
